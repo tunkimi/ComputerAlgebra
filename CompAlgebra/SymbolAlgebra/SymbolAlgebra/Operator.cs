8318f49caae4129a53f67d2cc589dcb1e9d761f7
tunkimi <57828177+tunkimi@users.noreply.github.com>
2023-02-27 00:49:53 +0300

    les1

---
+++ CompAlgebra/SymbolAlgebra/SymbolAlgebra/Operator.cs
@@ -0,0 +1,40 @@
+﻿namespace SymbolAlgebra
+{
+    interface Operator
+    {
+        public double Calc();
+        public bool Calculated { get; set; }
+        public List<Operator>? Operands { get; set; }
+        public double? Val { get; set; }
+    }
+    class Plus : Operator
+    {
+        public Plus(string param)
+        {
+
+        }
+        public double Calc()
+        {
+            double result = 0;
+            for (int i = 0; i < Operands.Count; i++)
+            {
+                result+= Operands[i].Calc();
+            }
+            return result;
+        }
+        public bool Calculated { get; set; } = false;
+        public List<Operator>? Operands { get; set; } = null;
+        public double? Val { get; set; } = null;
+    }
+    class Value : Operator
+    {
+        public Value(double val)
+        {
+            Val = val;
+        }
+        public double Calc() => (double)Val;
+        public bool Calculated { get; set; } = true;
+        public List<Operator>? Operands { get; set; } = null;
+        public double? Val { get; set; }
+    }
+}
---
+++ CompAlgebra/SymbolAlgebra/SymbolAlgebra/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using SymbolAlgebra;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        Value v1 = new Value(1);
+        Value v2 = new Value(2);
+        Value v3 = new Value(3);
+        Plus p1 = new Plus("") { Calculated = false, Val = null, Operands = new List<Operator> { v1, v2, v3 } };
+        Console.WriteLine(p1.Calc());
+        Console.ReadKey();
+    }
+}
